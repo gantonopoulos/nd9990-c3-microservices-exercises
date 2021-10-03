87047bb01bd68d6a1c150fb24db452e62bbecdf8
Georgios Antonopoulos <georanto@gmail.com>
2021-10-03 20:26:12 +0200

    Sets up the environment variables logic

--- project/udagram-api-feed/src/config/config.ts
+++ project/udagram-api-feed/src/config/config.ts
@@ -1,16 +1,16 @@
 export const config = {
-    "username": process.env["POSTGRES_USERNAME"],
-    "password": process.env["POSTGRES_PASSWORD"],
-    "database": process.env["POSTGRES_DATABASE"],
-    "host": process.env["POSTGRES_HOST"],
+    "username": process.env.POSTGRES_USERNAME,
+    "password": process.env.POSTGRES_PASSWORD,
+    "database": process.env.POSTGRES_DATABASE,
+    "host": process.env.POSTGRES_HOST,
     "dialect": "postgres",
-    "aws_region": process.env["AWS_REGION"],
-    "aws_profile": process.env["AWS_PROFILE"],
-    "aws_media_bucket": process.env["AWS_MEDIA_BUCKET"],
-    "image_filter_server": process.env["IMAGE_FILTER_SRV"],
-    "url": process.env["URL"],
+    "aws_region": process.env.AWS_REGION,
+    "aws_profile": process.env.AWS_PROFILE,
+    "aws_media_bucket": process.env.AWS_MEDIA_BUCKET,
+    "image_filter_server": process.env.IMAGE_FILTER_SRV,
+    "url": process.env,URL,
     "jwt":
         {
-          "secret":process.env["JWT_SECRET"]
+          "secret":process.env.JWT_SECRET
         }
 };
