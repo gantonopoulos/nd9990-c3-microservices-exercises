f85195cd8e3d5fa3f43bad1c134be28ed1436fe7
Georgios Antonopoulos <georanto@gmail.com>
2021-10-14 11:57:42 +0200

    Sets up the udagra-api-user's cluster configuration

--- project/udagram-api-user/src/config/config.ts
+++ project/udagram-api-user/src/config/config.ts
@@ -4,10 +4,7 @@ export const config = {
     "database": process.env.POSTGRES_DATABASE,
     "host": process.env.POSTGRES_HOST,
     "dialect": "postgres",
-    "aws_region": process.env.AWS_REGION,
     "aws_profile": process.env.AWS_PROFILE,
-    "aws_media_bucket": process.env.AWS_MEDIA_BUCKET,
-    "image_filter_server": process.env.IMAGE_FILTER_SRV,
     "url": process.env.URL,
     "jwt":
         {
